5db611f76657977bc761cbb87151fed18079cbe7
Lynn Soler <lsoler002@msn.com>
2019-02-03 16:35:11 -0700

    update test
    com2

--- Query.Library.sql
+++ Query.Library.sql
@@ -1,94 +1,97 @@
 
---Query for finding a book and what branch it's located at
-CREATE PROCEDURE uspGetBookBranch 
-@Title VARCHAR(50), 
-@BranchName VARCHAR(50)
+-- =FINDING BOOK + BRCH LOC
+CREATE PROC uspGETBookAndBranch 
+@Title VARCHAR(30), 
+@BranchName VARCHAR(30)
+--VARCHAR CONSIST CHK = OK
 AS
 	SELECT 
-	a1.books_title, a2.branch_name, c.number_of_copies
+	a1.books_Title, a2.branch_Name, c.number_Copies --2.) =HOW MANY COPIES 
 	FROM tbl_books a1
-	JOIN tbl_bookcopies c ON c.books_id = a1. books_id
-	INNER JOIN tbl_library a2 ON a2.branch_id = c.branch_id
-	WHERE books_title = @Title and branch_name = @BranchName
+	JOIN tbl_bookcopies c ON c.books_ID = a1. books_ID
+	INNER JOIN tbl_library a2 ON a2.branch_ID = c.branch_ID
+	WHERE books_Title = @Title and branch_Name = @BranchName
 GO
+EXEC uspGETBookAndBranch 'The Lost Tribe', 'Sharpstown' -- 1.) 2.) HOW MANY COPIES "THE LOST TRIBE" BKS_TITLE @ BRANCHNAME "SHARPSTOWN"
 
-EXEC uspGetBookBranch 'The Lost Tribe', 'Sharpstown'
 
 
---Query that retrieves the names of all borrowers who do not have any books checked out
-CREATE PROCEDURE uspZeroBooks
+
+--3.) RETRIEVE NAMES OF BORROWERS W/O ANY BKS CHECKED OUT
+CREATE PROC uspNoBooks
 AS
 	SELECT 
-	a1.borrower_name, a2.card_number, a2.date_out, a2. date_due
+	a1.borrower_Name, a2.card_Num, a2.date_Out, a2. date_Due
 	FROM tbl_borrower a1
-	LEFT JOIN tbl_bookloans a2 ON a1.card_number = a2. card_number
-	WHERE a1.card_number NOT IN (SELECT card_number FROM tbl_bookloans)
+	LEFT JOIN tbl_bookloans a2 ON a1.card_Num = a2. card_Num
+	WHERE a1.card_Num NOT IN (SELECT card_Num FROM tbl_bookloans) -- = W/OUT ANY BKS
+EXEC uspNoBooks
 
-EXEC uspZeroBooks
 
 
---Query that retrieves each book that is due today at different branch locations
-CREATE PROCEDURE uspBooksDueToday
-@BranchName VARCHAR(50), 
-@DateDue VARCHAR(50)
+
+--RETRIEVE EA BK DUE TODAY @ VARIOUS BRCH LOC'S
+CREATE PROC uspBooksDueToday --4.) CREATING DUETODAY PROC
+@BranchName VARCHAR(30), 
+@DateDue VARCHAR(10)
+--VARCHAR CONSIST CHK = OK
 AS
-
-	SELECT 
-	a1.branch_name, a2.date_due, c.books_title, d.borrower_name, d.borrower_address
+	SELECT --4.) RETRIEVE BK TITLE + BORROWER NAME + BORROWER ADDY  FOR EA BOOK FROM "Sharpstown" BRCH W/"DATEDUE" = TODAY 
+	a1.branch_Name, a2.date_Due, c.books_Title, d.borrower_Name, d.borrower_Addy
 	FROM tbl_library a1
-	JOIN tbl_bookloans a2 ON a2.branch_id = a1. branch_id
-	JOIN tbl_books c ON c.books_id = a2.books_id
-	JOIN tbl_borrower d ON a2.card_number = d.card_number
-	WHERE branch_name = @BranchName and date_due = @DateDue
-
-EXEC uspBooksDueToday 'Sharpstown', '10/22/2018'
-
+	JOIN tbl_bookloans a2 ON a2.branch_ID = a1. branch_ID
+	JOIN tbl_books c ON c.books_ID = a2.books_ID
+	JOIN tbl_borrower d ON a2.card_Num = d.card_Num
+	WHERE branch_Name = @BranchName and date_Due = @DateDue
+EXEC uspBooksDueToday 'Sharpstown', '10/31/2018'
 
 
---Query that retrieves the branch name and the total number of books loaned out from that branch
+
+--5.) RETRIEVE BRANCH NAME + TOTAL BKS CHECKED OUT FROM BRCH "Sharpstown"
 CREATE PROCEDURE uspTotalBookLoans
-@BranchName VARCHAR(50)
+@BranchName VARCHAR(30)
+--VARCHAR CONSIST CHK = OK
 AS
 	SELECT 
-	a1.branch_name, COUNT(books_title)
+	a1.branch_Name, COUNT(books_Title)
 	FROM tbl_library a1
-	JOIN tbl_bookloans a2 ON a2.branch_id = a1. branch_id
-	JOIN tbl_books c ON c.books_id = a2.books_id
+	JOIN tbl_bookloans a2 ON a2.branch_ID = a1. branch_ID
+	JOIN tbl_books c ON c.books_ID = a2.books_ID
 	WHERE branch_name = @BranchName
-	GROUP BY a1.branch_name
-	
+	GROUP BY a1.branch_Name
 EXEC uspTotalBookLoans 'Sharpstown'
+--EXEC "" '@BranchName'
 
 
 
---Query that retrieves the names, addresses, and the number of books checked out for all borrowers who have more than five books checked out
-CREATE PROCEDURE uspGreaterThanFiveBooks
+--6.) RETRIEVE NAMES + ADDYS + # BKS CHKD OUT FOR ALL BRRWERS WHO HAVE > 5 BKS CHKD OUT
+CREATE PROCEDURE uspGREATERThanFiveBooks
 AS
-	SELECT borrower_name, c.card_number, c.borrower_address FROM
+	SELECT borrower_Name, c.card_Num, c.borrower_Addy FROM
 	tbl_books a 
-	JOIN tbl_bookloans b ON a.books_id = b.books_id
-	JOIN tbl_borrower c ON c.card_number = b.card_number
-	GROUP BY borrower_name, c.card_number, c.borrower_address
-	HAVING COUNT(b.card_number) > 5
-	ORDER BY c.card_number ASC
-
-EXEC uspGreaterThanFiveBooks
-
+	JOIN tbl_bookloans b ON a.books_ID = b.books_ID
+	JOIN tbl_borrower c ON c.card_Num = b.card_Num
+	GROUP BY borrower_Name, c.card_Num, c.borrower_Addy
+	HAVING COUNT(b.card_Num) > 5 -- = BORROWER ID W/ >5 BOOKS CHKD OUT
+	ORDER BY c.card_Num ASC
+EXEC uspGREATERThanFiveBooks
 
 
 
 
---Query that retrieves the number of copies owned by a specific library branch
-CREATE PROCEDURE uspNumberOfCopiesAndBranch
-@Title VARCHAR(50), 
-@BranchName VARCHAR(50)
+
+--RETRIEVE # OF COPIES OWNED BY BRCH
+CREATE PROCEDURE uspNumCopiesAtBranch
+@Title VARCHAR(30), 
+@BranchName VARCHAR(30)
+-- VARCHAR CONSIST CHK = OK
 AS
 
-	SELECT authors_name, books_title, number_of_copies, branch_name 
+	SELECT authors_Name, books_Title, num_Copies, branch_Name 
 		FROM tbl_books a
-		JOIN tbl_authors e ON e.books_id = a.books_id
-		JOIN tbl_bookcopies b ON  a.books_id = b.books_id
-		JOIN tbl_library d ON d.branch_id = b.branch_id
-		WHERE books_title = @Title and branch_name = @BranchName
+		JOIN tbl_authors e ON e.books_ID = a.books_ID
+		JOIN tbl_bookcopies b ON  a.books_ID = b.books_ID
+		JOIN tbl_library d ON d.branch_ID = b.branch_ID
+		WHERE books_Title = @Title and branch_Name = @BranchName
 
-EXEC uspNumberofCopiesAndBranch 'Misery', 'Central'
+EXEC uspNumCopiesAtBranch 'IT', 'Central'
